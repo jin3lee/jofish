8b22fabe22f67e14a609a694df3b58aa7b4bbb35
LEEJ431 <Joseph.X.Lee.-ND@disney.com>
2017-04-02 22:06:43 -0700

    regenerated files without other platforms like ios/desktop

--- core/src/com/jofish/game/JoFish.java
+++ core/src/com/jofish/game/JoFish.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class JoFishGame extends ApplicationAdapter {
+public class JoFish extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
