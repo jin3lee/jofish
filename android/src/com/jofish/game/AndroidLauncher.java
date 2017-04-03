1d13d5601accc3a4398164f225d695ad66fbb3db
LEEJ431 <Joseph.X.Lee.-ND@disney.com>
2017-04-02 21:15:49 -0700

    generated project with libgdx project generator

---
+++ android/src/com/jofish/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jofish.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jofish.game.JoFishGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new JoFishGame(), config);
+	}
+}
